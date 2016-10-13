ddf09c1ad37b143f9731c7f60af6f07a7bc49215
Alexis Hassler <alexis.hassler@sewatech.fr>
2016-10-13 23:56:46 +0200

    Fix maven & bash builds

--- scripts/prepare-jigsaw.sh
+++ scripts/prepare-jigsaw.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 source `dirname $0`/setenv.sh
 
+version=`java -version 2>&1 | awk -F '"' '/version/ {print $2}'`
+if [[ "$version" != "9-ea" ]]; then
+    echo Java version is not 9
+    exit
+fi
+
 cd $src_home
 mvn clean install -Pjigsaw
 
@@ -8,5 +14,5 @@ rm -rf $demo_home/jigsaw-modules
 mkdir -p $demo_home/jigsaw-modules
 
 cp $src_home/message-launcher/target/*.jar $demo_home/jigsaw-modules/
-cp $src_home/message-launcher/target/dependency/*.jar $demo_home/jigsaw-modules/
+cp $dep_dir/*.jar $demo_home/jigsaw-modules/
 cp $src_home/misc-examples/target/*.jar $demo_home/jigsaw-modules/
--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-script_home=`dirname $0`
-src_home=`dirname $script_home`
-demo_home=`dirname $src_home`/classpath-demo-work/
-m2_repo=~/.m2/repository/
+script_home=$(realpath $(dirname $0))
+src_home=$(dirname $script_home)
+dep_dir=$src_home/message-launcher/target/dependency
+demo_home=$(dirname $src_home)/classpath-demo-work
+m2_repo=~/.m2/repository
